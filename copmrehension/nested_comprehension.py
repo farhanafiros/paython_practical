59a427d8419e9e511d4433d5e3429fbb0b2a96c4
farhana <farhanafiros541@gemail.com>
2026-09-03 04:40:04 +0530

    comprihenstion nested  created

---
+++ copmrehension/nested_comprehension.py
@@ -0,0 +1,7 @@
+matrix = [[1,2],[3,4],[5,6]]
+flat = [num for row in matrix for num in row]
+print(flat)
+
+#multipliation table players
+pairs = [(x,y)for x in range(1,4)for y in range(1,4)]
+print(pairs)
---
+++ copmrehension/tuple_comprehenstion.py
@@ -0,0 +1,3 @@
+gen = (x**2 for x in range(5))
+print(gen)
+print(tuple(gen))
