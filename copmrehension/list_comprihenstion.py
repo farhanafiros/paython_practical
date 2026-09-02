f587bc3f1be1ab192a634c750ddd0e7cb172f53e
farhana <farhanafiros541@gemail.com>
2026-09-03 03:02:05 +0530

    comprihenstion list  created

--- copmrehension/list_comprihenstion.py
+++ copmrehension/list_comprihenstion.py
@@ -6,3 +6,11 @@ print(squares)
 
 squares_comp = [x**2 for x in range(1,6)]
 print(squares_comp)
+
+#evev numbers
+even_numbers = [x for x in range (1,6)if x % 2 ==0 ]
+print(even_numbers)
+
+
+letter = [ch.upper() for ch in "hello"]
+print(letter)
